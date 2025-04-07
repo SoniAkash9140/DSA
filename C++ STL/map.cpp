7d63192a379259c3f86bd24fcdb6c6b63bda24cd
Akash Soni <as6171079@gmail.com>
2025-04-07 22:47:35 +0530

    first commit

--- C++ STL/map.cpp
+++ C++ STL/map.cpp
@@ -1,8 +1,9 @@
+<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 
 void explainMap() {
-    // Use a single map declaration
+    // map-> unique key and values in sorted order. It contains {key,values}
     map<int, int> mpp;
 
     // Adding key-value pairs to the map
@@ -41,3 +42,32 @@ int main() {
     explainMap();
     return 0;
 }
+=======
+//map-> uniqe key and values in sorted order. It contains {key,values}
+#include<bits/stdc++.h>
+using namespace std ;
+
+void explainMap(){
+    map< int, int> mpp;
+    map<int , pair<int,int>>mpp;
+    map<pair <int,int> ,int>mpp;
+
+    mpp[1]=2;
+    mpp.emplace({3,1});
+
+    mpp.insert({2,4});
+    mpp[{2,3}]=10;
+
+    for(auto it:mpp){
+        cout << it.first<< " " << it.second << end;
+    }
+    
+    cout << mpp[1];
+    cout << mpp[5];
+
+    auto it =mpp.find(3);
+    cout << *(it).second;
+
+    auto it =mpp.find(5);
+}
+>>>>>>> f7acfe6 (first commit)
