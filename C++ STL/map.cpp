165701dc433df092fac38a198ed8d80fb7271aaf
Akash Soni <as6171079@gmail.com>
2025-08-29 12:01:05 +0530

    Clean and fix map.cpp

--- C++ STL/map.cpp
+++ C++ STL/map.cpp
@@ -1,28 +1,28 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 
 void explainMap() {
-    // Use a single map declaration
+    // declare a map (unique keys, sorted order)
     map<int, int> mpp;
 
-    // Adding key-value pairs to the map
-    mpp[1] = 2;
-    mpp.emplace(3, 1);
-    mpp.insert({2, 4});
+    // insert values
+    mpp[1] = 2;          // key=1, value=2
+    mpp.emplace(3, 1);   // key=3, value=1
+    mpp.insert({2, 4});  // key=2, value=4
 
-    // Iterating over the map
+    // iterate
+    cout << "Map elements:" << endl;
     for (auto it : mpp) {
         cout << it.first << " " << it.second << endl;
     }
 
-    // Accessing elements
+    // access
     cout << "Value at key 1: " << mpp[1] << endl;
 
-    // Attempting to access a non-existent key
+    // if key not present → default value (0)
     cout << "Value at key 5 (default): " << mpp[5] << endl;
 
-    // Finding elements in the map
+    // find an element
     auto it = mpp.find(3);
     if (it != mpp.end()) {
         cout << "Found key 3 with value: " << it->second << endl;
@@ -42,32 +42,3 @@ int main() {
     explainMap();
     return 0;
 }
-=======
-//map-> uniqe key and values in sorted order. It contains {key,values}
-#include<bits/stdc++.h>
-using namespace std ;
-
-void explainMap(){
-    map< int, int> mpp;
-    map<int , pair<int,int>>mpp;
-    map<pair <int,int> ,int>mpp;
-
-    mpp[1]=2;
-    mpp.emplace({3,1});
-
-    mpp.insert({2,4});
-    mpp[{2,3}]=10;
-
-    for(auto it:mpp){
-        cout << it.first<< " " << it.second << end;
-    }
-    
-    cout << mpp[1];
-    cout << mpp[5];
-
-    auto it =mpp.find(3);
-    cout << *(it).second;
-
-    auto it =mpp.find(5);
-}
->>>>>>> f7acfe6 (first commit)
