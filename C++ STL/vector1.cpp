bdc77b3c4603acbf8a753bcfebb41c4c6ac8ad3d
Akash Soni <as6171079@gmail.com>
2025-11-03 19:45:08 +0530

    first commit

--- C++ STL/vector1.cpp
+++ C++ STL/vector1.cpp
@@ -1,42 +1,40 @@
 #include<bits/stdc++.h>
-using  namespace std;
-int main(){
-    vector <int> v ;
-    cout << "Capacity->" << v.capacity() << endl;
 
-    //Push
-    v.push_back(1);
-    cout << "Capacity->" << v.capacity() << endl;
-    v.push_back(2);
-    cout << "Capacity->" << v.capacity() << endl;
-    v.push_back(3);
-    cout << "Capacity->" << v.capacity() << endl;
-    cout << "size->" << v.size() << endl;
+using namespace std;
 
-    cout << "element at 2nd Inde" << v.at(2) << endl;
+int main() {
+  vector < int > v;
 
-    cout << "Front->" << v.front() << endl;
-    cout << "Back->" << v.back() << endl;
+  for (int i = 0; i < 10; i++) {
+    v.push_back(i); //inserting elements in the vector
+  }
 
-// Pop
-    cout << "Before Pop" << endl;
-    for(int i:v){
-        cout << i << " ";
-    } cout << endl;
+  cout << "the elements in the vector: ";
+  for (auto it = v.begin(); it != v.end(); it++)
+    cout << * it << " ";
 
-    v.pop_back();
-    cout << "after pop" << endl;
-    for(int i:v){
-        cout << i << " ";
-    }
+  cout << "\nThe front element of the vector: " << v.front();
+  cout << "\nThe last element of the vector: " << v.back();
+  cout << "\nThe size of the vector: " << v.size();
+  cout << "\nDeleting element from the end: " << v[v.size() - 1];
+  v.pop_back();
 
-    vector<int> a(5,1) ;
-    
+  cout << "\nPrinting the vector after removing the last element:" << endl;
+  for (int i = 0; i < v.size(); i++)
+    cout << v[i] << " ";
 
-    // copy last vector
-    vector<int> last(a);
-    cout <<"print a" << endl;
-    for(int i:a){
-        cout << i << " ";
-    }
+  cout << "\nInserting 5 at the beginning:" << endl;
+  v.insert(v.begin(), 5);
+  cout << "The first element is: " << v[0] << endl;
+  cout << "Erasing the first element" << endl;
+  v.erase(v.begin());
+  cout << "Now the first element is: " << v[0] << endl;
+
+  if (v.empty())
+    cout << "\nvector is empty";
+  else
+    cout << "\nvector is not empty" << endl;
+
+  v.clear();
+  cout << "Size of the vector after clearing the vector: " << v.size();
 }
