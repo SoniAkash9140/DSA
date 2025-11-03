62a874b242980f7a9885de99063180033d73ca57
Akash Soni <as6171079@gmail.com>
2025-11-03 19:47:50 +0530

    first commit

--- C++ STL/Sorting/decreasing.cpp
+++ C++ STL/Sorting/decreasing.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>  // For std::sort
+using namespace std;
 
 int main() {
-    std::vector<int> v = {5, 2, 8, 1, 3};
+    vector<int> v = {5, 2, 8, 1, 3};
 
     // Sorting in descending order using a lambda function
-    std::sort(v.begin(), v.end(), [](int a, int b) {
+    sort(v.begin(), v.end(), [](int a, int b) {
         return a > b;  // Change the comparison for descending order
     });
 
     for (int num : v) {
-        std::cout << num << " ";
+        cout << num << " ";
     }
-    std::cout << std::endl;
+    cout <<endl;
 
     return 0;
 }
